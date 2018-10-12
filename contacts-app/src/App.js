1bee350d9fe7b6a0c6f1deb61209c329f74204fc
Ana María <anamariasosam@gmail.com>
2018-10-11 21:32:38 -0500

    create contacts app

--- contacts-app/src/App.js
+++ contacts-app/src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			contacts: [],
+			favorites: [],
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://randomuser.me/api/?results=10')
+		.then( result => result.json() )
+		.then(data => {
+			this.setState({
+				contacts: data.results,
+			})
+		})
+	}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+				<ContactsList 
+					contacts={this.state.contacts}
+					title="Contacts"
+				/>
+				<ContactsList 
+					contacts={this.state.favorites}
+					title="Favorites"
+				/>
       </div>
     );
   }
+}
+
+const ContactsList = (props) => {
+	return (
+		<div>
+			{
+				props.contacts.map( contact => <ContactCard contact={contact} key={contact.email}/>)
+			}
+		</div>
+	)
+}
+
+class ContactCard extends Component {	
+	render() {
+		return (
+			<div>
+				<figure>
+					<img 
+						src={this.props.contact.picture.large} 
+						alt="author" />
+					<figcaption>{this.props.contact.name.first}</figcaption>
+				</figure>
+				<button>Favorito</button>
+				<button>Remover</button>
+			</div>
+		)
+	}
 }
 
 export default App;
