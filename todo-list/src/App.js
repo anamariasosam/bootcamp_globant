75a59ffc82252c560b5234da27f47c1deb649a39
Ana María <anamariasosam@gmail.com>
2018-10-17 20:51:09 -0500

    edit build script and rename router component

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import TodoList from './views/TodoList'
 import CategoryList from './views/CategoryList'
 import './styles/App.css'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <div className="main">
         <Route path="/" component={CategoryList} exact />
-        <Route path="/todo/:categoryName" component={TodoList} exact />
+        <Route path="/todo/:categoryName" component={TodoList} />
       </div>
-    </BrowserRouter>
+    </Router>
   )
 }
 
