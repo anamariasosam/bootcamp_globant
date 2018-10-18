fee7c66a0929e48b5d9ee1a2e074fb58ab0b4da7
Ana María <anamariasosam@gmail.com>
2018-10-17 19:08:16 -0500

    Add images, styles and category data

---
+++ todo-list/src/data/data.js
@@ -0,0 +1,26 @@
+export const categories = [
+	{
+		name: "Work",
+		image: require('../images/work.png')
+	},
+	{
+		name: "University",
+		image: require('../images/university.png')
+	},
+	{
+		name: "Learn",
+		image: require('../images/learn.png')
+	},
+	{
+		name: "House",
+		image: require('../images/house.png')
+	},
+	{
+		name: "Doctor",
+		image: require('../images/doctor.png')
+	},
+	{
+		name: "Others",
+		image: require('../images/others.png')
+	}
+]
